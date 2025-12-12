98bf85899f43943b53052eceb137210ff307f1f6
hexatronn <hikmtmmmdov26@gmail.com>
2025-12-12 15:41:55 +0300

    Insert a document to Python

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Mongo insert"""
-
-
 def insert_school(mongo_collection, **kwargs):
+    """inserts new document"""
     return mongo_collection.insert_one(kwargs).inserted_id
